7d21bb489027f006b9eca264f3e2bc31b76d733d
FelipeApol <apolinariofelipe133@gmail.com>
2026-09-25 09:40:02 -0300

    atualizando

---
+++ exercicio_2.py
@@ -0,0 +1,8 @@
+ ''''Verificar palíndromo
+
+Escreva um programa que verifica se uma string é um palíndromo.
+'''
+frase = input("Digite uma frase: ")
+palavras = frase.split()
+quantidade = len(palavras)
+print(f'A frase contem {quantidade} palavras.')
